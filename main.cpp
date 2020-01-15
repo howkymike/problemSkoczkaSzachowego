051695cb0290e106288ac1524e9431736c5ce255
Michał <34208123+howkymike@users.noreply.github.com>
2020-01-15 18:26:12 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -40,10 +40,10 @@ int main(int argc, char* argv[])
 
 	// jezeli nie znaleziono drogi od (x0,y0) - wypisac
 	// "** Nie ma możliwości odwiedzić jjednoczescie każdego pola!!!\n\n";
-	// jesli ok to wypisac szachownice z numerami kolejnych krokow
 	int rootReturned = root(board, CHESSBOARD_SIZE, 1, x0, y0, HorseMove);
 	if(rootReturned == 0)
 		printf("You  can NOT visit every single cell just once!!!\n\n");
+	// jesli ok to wypisac szachownice z numerami kolejnych krokow
 	else
 		printChessBoard(board, CHESSBOARD_SIZE);
 
@@ -51,81 +51,4 @@ int main(int argc, char* argv[])
 	freeChessboard(&board);
 
 	return 0;
-}
-
-
-// jezeli mozna sie ruszyc to zwracam 1, jezeli nie da sie to 0
-int move(int** pChessBoard, int nDim, int move, int x, int y, int* px, int* py, Point* pHorseMoves)
-{
-	*px = x + pHorseMoves[move].x;
-	*py = y + pHorseMoves[move].y;
-	if( *px < nDim && *px >= 0 && *py < nDim && &py >= 0
-		&& pChessBoard[*px][*py] == 0 )
-		return 1;
-	return 0;
-
-}
-
-int root(int** pChessBoard, int nDim, int moveNo, int x, int y, Point* pHorseMoves)
-{
-	// tabl pChessBoard pamieta droge - 0 pole odiwedzone 1..nDim^2 numer kolejnego ruchu
-
-	//wstawic kolejny numer ruchu
-	//jesli koniec to zwracamy 1
-	//else
-		//zdefiniowac wszystkie mozliwosci ruchu
-		//jesli ruch do nowej pozycji mozliwy
-		// to rekurencyjnie wykonac ruch moveNo+1
-		// jesli zwrocono 1 z rekurencji to zwrocic 1
-	// jezeli wracamy to ustawiamy w pChessBoard[x][y] na 0 bo ten ruch byl zly
-	pChessBoard[x][y] = moveNo;
-
-	if( moveNo == nDim * nDim )
-		return 1;
-	
-	int newX, newY;
-	int moveIndex = 0;
-	while( moveIndex < HORSE_MOVES )
-	{
-		if( move(pChessBoard, nDim, moveIndex, x, y, &newX, &newY, pHorseMoves) == 1 )
-		{
-			int rootReturn = root(pChessBoard, nDim, moveNo + 1, newX, newY, pHorseMoves);
-			if( rootReturn == 1 )
-				return 1;
-			pChessBoard[newX][newY] = 0;
-		}
-		moveIndex++;
-	}
-	return 0;
-}
-
-int** createChessboard(int nDim)
-{
-	int** rows = (int**)calloc(nDim, sizeof(int*));
-	if( !rows )
-		return NULL;
-	for( int i = 0; i < nDim; i++ )
-	{
-		rows[i] = (int*)calloc(nDim, sizeof(int));
-		if( !rows[i] )
-			return NULL;
-	}
-	return rows;
-}
-
-void printChessBoard(int** pChessBoard, int nDim)
-{
-	for( int i = 0; i < nDim; i++ )
-	{
-		for( int j = 0; j < nDim; j++ )
-			printf("%d ", pChessBoard[i][j]);
-		printf("\n");
-	}
-}
-
-void freeChessboard(int*** pChessBoard)
-{
-	free(*pChessBoard[0]);
-	free(*pChessBoard);
-	*pChessBoard = NULL;
 }
